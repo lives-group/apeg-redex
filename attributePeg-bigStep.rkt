6021c9b52e4d53402bb257430d75e8cd9f002fdf
Regina Sarah <monferrari.regina@estudante.ufjf.br>
2022-08-31 19:45:55 -0300

    more bigstep apeg

--- attributePeg-bigStep.rkt
+++ attributePeg-bigStep.rkt
@@ -15,14 +15,94 @@
    --------------------------------
    (eval ctx G (natural_1 (natural_2 natural ...)) ⊥)]
 
+  [--------------------------------
+   (eval ctx G (natural_1 ()) ⊥)]
+
+  ;Choice 
+  [(eval ctx G (p_1 r) (natural ...))
+   --------------------------------
+   (eval ctx G ((/ p_1 p_2) r) (natural ...))]
+
+  [(eval ctx G (p_1 r) ⊥)
+   (eval ctx G (p_2 r) r_1)
+   -------------------------------
+   (eval ctx G ((/ p_1 p_2) r) r_1)]
+
+  ;Sequence
+  [(eval ctx G (p_1 r) (natural ...))
+   (eval ctx G (p_2 (natural ...)) r_2)
+   -------------------------------
+   (eval ctx G ((• p_1 p_2) r) r_2)]
+
+  [(eval ctx G (p_1 r) ⊥)
+   ------------------------------
+   (eval ctx G ((• p_1 p_2) r) ⊥)]
+
+  ;Not
+  [(eval ctx G (p r) (natural ...) )
+   -------------------------------
+   (eval ctx G ((! p) r) ⊥)]  
+
+  [(eval ctx G (p r) ⊥)
+   -------------------------------
+   (eval ctx G ((! p) r) r)]
+
+  ;Repetition
+  [
+   -------------------------------
+   (eval ctx G ((* ε) r) ⊥)]
+  
+  [(eval ctx G (p r) ⊥)
+   -------------------------------
+   (eval ctx G ((* p) r) r)]
+
+  [(eval ctx G (p r) (natural ...))
+   (eval ctx G ((* p) (natural ...)) r_2)
+   -------------------------------
+   (eval ctx G ((* p) r) r_2)]
+
+  ;Empty
+  [-------------------------------
+   (eval ctx G (ε r) r)]
+
+
 )
 
 (define-metafunction val-AttributePeg
   [(dismatch? natural_1 natural_1) #f]
   [(dismatch? natural_1 natural_2) #t]) 
 
+;TERMINAL
 (judgment-holds (eval () ∅ (1 (1 2)) r) r)
 (judgment-holds (eval () ∅ (1 (2 2)) r) r)
+(judgment-holds (eval () ∅ (1 ()) r) r)
+;CHOICE
+(judgment-holds (eval () ∅ ((/ 1 2) (1 2)) r) r)
+(judgment-holds (eval () ∅ ((/ 1 2) (2 1)) r) r)
+(judgment-holds (eval () ∅ ((/ 1 2) (3 3)) r) r)
+(judgment-holds (eval () ∅ ((/ 1 2) ()) r) r)
+;SEQUENCE
+(judgment-holds (eval () ∅ ((• 1 2) (1 2)) r) r)
+(judgment-holds (eval () ∅ ((• 1 2) (1 2 2)) r) r)
+(judgment-holds (eval () ∅ ((• 1 2) (2 2)) r) r)
+(judgment-holds (eval () ∅ ((• 1 2) ()) r) r)
+;NOT
+(judgment-holds (eval () ∅ ((! 1) (1)) r) r)
+(judgment-holds (eval () ∅ ((! 1) (2)) r) r)
+(judgment-holds (eval () ∅ ((! 1) ()) r) r)
+(judgment-holds (eval () ∅ ((! 1) (1 2)) r) r)
+;REPETITION
+(judgment-holds (eval () ∅ ((* 1) (1 1 1 1 2 3)) r) r)
+(judgment-holds (eval () ∅ ((* 1) ()) r) r)
+;(judgment-holds (eval () ∅ ((* ε) (1 2)) r) r) ;DA RUIM
+;EMPTY
+(judgment-holds (eval () ∅ (ε (1 2)) r) r)
+;NON-TERMINAL
+;--
+;MIX
+;(judgment-holds (eval () ∅ ((* (/ (• 1 2) (! 3))) (1 2 1 2)) r) r)
+(judgment-holds (eval () ∅ ((/ (• 1 2) (! 3)) (1 2 3)) r) r)
+(judgment-holds (eval () ∅ ((/ (• 1 2) (! 3)) (4)) r) r)
 
 
 
