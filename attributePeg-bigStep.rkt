193905b4d9808c717d6c23448d05191cf85aafa1
dfreita4s <danifp19@gmail.com>
2022-09-20 16:54:14 -0300

    testes ->

--- attributePeg-bigStep.rkt
+++ attributePeg-bigStep.rkt
@@ -86,41 +86,49 @@
 
 
 ;TERMINAL
-(judgment-holds (parse () ∅ (1 (1 2)) r ctx) r)
-(judgment-holds (parse () ∅ (1 (2 2)) r ctx) r)
-(judgment-holds (parse () ∅ (1 ()) r ctx) r)
-;CHOICE
-(judgment-holds (parse () ∅ ((/ 1 2) (1 2)) r ctx) r)
-(judgment-holds (parse () ∅ ((/ 1 2) (2 1)) r ctx) r)
-(judgment-holds (parse () ∅ ((/ 1 2) (3 3)) r ctx) r)
-(judgment-holds (parse () ∅ ((/ 1 2) ()) r ctx) r)
-;SEQUENCE
-(judgment-holds (parse () ∅ ((• 1 2) (1 2)) r ctx) r)
-(judgment-holds (parse () ∅ ((• 1 2) (1 2 2)) r ctx) r)
-(judgment-holds (parse () ∅ ((• 1 2) (2 2)) r ctx) r)
-(judgment-holds (parse () ∅ ((• 1 2) ()) r ctx) r)
-;NOT
-(judgment-holds (parse () ∅ ((! 1) (1)) r ctx) r)
-(judgment-holds (parse () ∅ ((! 1) (2)) r ctx) r)
-(judgment-holds (parse () ∅ ((! 1) ()) r ctx) r)
-(judgment-holds (parse () ∅ ((! 1) (1 2)) r ctx) r)
-;REPETITION
-(judgment-holds (parse () ∅ ((* 1) (1 1 1 1 2 3)) r ctx) r)
-(judgment-holds (parse () ∅ ((* 1) ()) r ctx) r)
+;(judgment-holds (parse () ∅ (1 (1 2)) r ctx) r)
+;(judgment-holds (parse () ∅ (1 (2 2)) r ctx) r)
+;(judgment-holds (parse () ∅ (1 ()) r ctx) r)
+;; CHOICE
+;(judgment-holds (parse () ∅ ((/ 1 2) (1 2)) r ctx) r)
+;(judgment-holds (parse () ∅ ((/ 1 2) (2 1)) r ctx) r)
+;(judgment-holds (parse () ∅ ((/ 1 2) (3 3)) r ctx) r)
+;(judgment-holds (parse () ∅ ((/ 1 2) ()) r ctx) r)
+;; SEQUENCE
+;(judgment-holds (parse () ∅ ((• 1 2) (1 2)) r ctx) r)
+;(judgment-holds (parse () ∅ ((• 1 2) (1 2 2)) r ctx) r)
+;(judgment-holds (parse () ∅ ((• 1 2) (2 2)) r ctx) r)
+;(judgment-holds (parse () ∅ ((• 1 2) ()) r ctx) r)
+;; NOT
+;(judgment-holds (parse () ∅ ((! 1) (1)) r ctx) r)
+;(judgment-holds (parse () ∅ ((! 1) (2)) r ctx) r)
+;(judgment-holds (parse () ∅ ((! 1) ()) r ctx) r)
+;(judgment-holds (parse () ∅ ((! 1) (1 2)) r ctx) r)
+;; REPETITION
+;(judgment-holds (parse () ∅ ((* 1) (1 1 1 1 2 3)) r ctx) r)
+;(judgment-holds (parse () ∅ ((* 1) ()) r ctx) r)
 ;(judgment-holds (parse () ∅ ((* ε) (1 2)) r ctx) r) ;DA RUIM
-;EMPTY
-(judgment-holds (parse () ∅ (ε (1 2)) r ctx) r)
+; EMPTY
+;(judgment-holds (parse () ∅ (ε (1 2)) r ctx) r)
 ;NON-TERMINAL
 ;--
 
 ;UPDATE
 (judgment-holds (parse ((x 1) (y 2)) ∅ (((← x 3)) (1 1 1)) s ctx) ctx)
 (judgment-holds (parse ((x 1) (y 2)) ∅ (((← x (+ 1 2))) (1 1 1)) s ctx) (s ctx))
+(judgment-holds (parse ((x 1) (y 2)) ∅ (((← x (* 1 2))) (1 1 1)) s ctx) (s ctx))
+;(judgment-holds (parse ((x 1) (y 2)) ∅ (((← x (/ 1 2))) (1 1 1)) s ctx) (s ctx)) ;;n funciona
+(judgment-holds (parse ((x 1) (y 2)) ∅ (((← x (+ (+ 1 2) 2))) (1 1 1)) s ctx) (s ctx))
+(judgment-holds (parse ((x 1) (y 2)) ∅ (((← x (+ (+ 1 2) (- 1 6)))) (1 1 1)) s ctx) (s ctx))
+;(judgment-holds (parse ((x 1) (y 2)) ∅ (((← x (+ (+ 1 2) (- 1 6))) (← y (+ (+ 1 2) (- 1 6)))) (1 1 1)) s ctx) (s ctx)) ;; nao funciona
+;(judgment-holds (parse ((x 1) (y 2)) ∅ (((← y (+ 5 2))) (1 1 1)) s ctx) (s ctx)) ;; nao funciona
+(judgment-holds (parse ((x 1) (y 2)) ∅ (((← z (+ 2 2))) (1 1 1)) s ctx) (s ctx)) ;; nao sei se eh pra ter esse comportamento
+
 ;;testar e estudar o artigo pra veer como vai fazer o terminal
 ;MIX
-(judgment-holds (parse () ∅ ((* (/ (• 1 2) 3)) (1 2 1 2)) r ctx) r)
-(judgment-holds (parse () ∅ ((/ (• 1 2) (! 3)) (1 2 3)) r ctx) r)
-(judgment-holds (parse () ∅ ((/ (• 1 2) (! 3)) (4)) r ctx) r)
+;(judgment-holds (parse () ∅ ((* (/ (• 1 2) 3)) (1 2 1 2)) r ctx) r)
+;(judgment-holds (parse () ∅ ((/ (• 1 2) (! 3)) (1 2 3)) r ctx) r)
+;(judgment-holds (parse () ∅ ((/ (• 1 2) (! 3)) (4)) r ctx) r)
 
 
 
